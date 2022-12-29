56ff0c795708760b364a026a190e12f76cd1fccd
Bayezid Mostafa <hiperx1@gmail.com>
2022-12-29 15:18:56 +0600

    Data Collection Created & First API Created

--- index.js
+++ index.js
@@ -13,6 +13,28 @@ app.get('/', (req, res) => {
     res.send('Theme Portal Server in up!')
 })
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.DB_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const run = async () => {
+    try{
+        const themeCollection = client.db('TPData').collection('themes');
+        app.get('/themes', async(req, res) => {
+            const query = {}
+            const result = await themeCollection.find(query).toArray();
+            res.send(result);
+        })
+    }
+    catch{}
+    finally{}
+}
+run().catch(er => {
+    console.error(err)
+})
+
+
+// Port Listener
 app.listen(port, () => {
     console.log(`Theme portal server is running on port number: ${port}`);
 })
